8df789249f5dd2d2a159f34868c71fad9dfc4676
Abdullah30nov <abdullahbinaslam64@gmail.com>
2025-05-08 01:07:04 +0500

    View and Link Count API

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+// Vercel ke liye port nahi define karte
+module.exports = server;
